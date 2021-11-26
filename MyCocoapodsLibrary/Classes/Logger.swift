b2b119e581a8c1f34e7056a8813b058b039fec40
Edward Sagayaraj <edward.christuraj@comvivia.com>
2021-11-26 14:19:45 +0530

    Initial Commit

---
+++ MyCocoapodsLibrary/Classes/Logger.swift
@@ -0,0 +1,20 @@
+//
+//  Logger.swift
+//  MyCocoapodsLibrary
+//
+//  Created by Edward Sagayaraj on 26/11/21.
+//
+
+import Foundation
+
+public class Logger {
+    public init() {}
+    
+    public func printLog() {
+        print("Hello World");
+    }
+    
+    func privateMethod() {
+        print("private");
+    }
+}
